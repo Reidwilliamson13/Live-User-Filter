40e9f11e4fad1f0ea657f70227f5741d2ad20118
ReidWilliamson <reidwilliamson2@gmail.com>
2021-11-10 11:32:11 -0500

    add js variables querySelector for grabbing

---
+++ index.js
@@ -0,0 +1,28 @@
+// Variables for main inputter section //
+const searchInput = document.querySelector("#school-search-input");
+const websiteAutopopulateBox = document.querySelector("#website-autopopulate");
+const collegeTownInput = document.querySelector("#college-town-input");
+const schoolSafetyInput = document.querySelector("#safety-input");
+const classSizeInput = document.querySelector("#class-size-input");
+const HrsFromHomeInput = document.querySelector("#hours-from-home-input");
+
+// Variables for autopopulated #1 ranked school //
+const collegeTown1 = document.querySelector("#college-town1");
+const safety1 = document.querySelector("#safety1");
+const classSize1 = document.querySelector("#class-size1");
+const hrsFromHome1 = document.querySelector("#hours-from-home1");
+const website1 = document.querySelector("#college-website1");
+
+// Variables for autopopulated #2 ranked school //
+const collegeTown2 = document.querySelector("#college-town2");
+const safety2 = document.querySelector("#safety1");
+const classSize2 = document.querySelector("#class-size2");
+const hrsFromHome2 = document.querySelector("#hours-from-home2");
+const website2 = document.querySelector("#college-website2");
+
+// Variables for autopopulated #3 ranked school //
+const collegeTown3 = document.querySelector("#college-town3");
+const safety3 = document.querySelector("#safety3");
+const classSize3 = document.querySelector("#class-size3");
+const hrsFromHome3 = document.querySelector("#hours-from-home3");
+const website3 = document.querySelector("#college-website3");
