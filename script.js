1389efc69c47576efd07fc26d8b6c8d655f7ef00
ReidWilliamson <reidwilliamson2@gmail.com>
2021-11-19 12:01:43 -0500

    Update with DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const result = document.getElementById("result");
-const filter = document.getElementById("filter");
-const listItems = [];
-const bottomContent = document.getElementById("content");
-const showMoreBox = document.getElementById("show-more");
+document.addEventListener("DOMContentLoaded", function () {
+  const result = document.getElementById("result");
+  const filter = document.getElementById("filter");
+  const listItems = [];
+  const bottomContent = document.getElementById("content");
+  const showMoreBox = document.getElementById("show-more");
 
-getData();
+  getData();
 
-filter.addEventListener("input", (e) => filterData(e.target.value));
+  filter.addEventListener("input", (e) => filterData(e.target.value));
 
-async function getData() {
-  const res = await fetch("https://randomuser.me/api?results=50");
-  const { results } = await res.json();
+  async function getData() {
+    const res = await fetch("https://randomuser.me/api?results=50");
+    const { results } = await res.json();
 
-  //Clear result
-  result.innerHTML = "";
+    //Clear result
+    result.innerHTML = "";
 
-  results.forEach((user) => {
-    const li = document.createElement("li");
+    results.forEach((user) => {
+      const li = document.createElement("li");
 
-    listItems.push(li);
-    //using innerHTML to modify the content of the HTML element, in this case, each li tag//
-    li.innerHTML = `
+      listItems.push(li);
+      //using innerHTML to modify the content of the HTML element, in this case, each li tag//
+      li.innerHTML = `
       <img src="${user.picture.large}" alt="${user.name.first}">
 <div class="user-info"> 
 <h4>${user.name.first} ${user.name.last}</h4>
@@ -28,30 +29,31 @@ async function getData() {
 </div>
       `;
 
-    result.appendChild(li);
-  });
-}
-
-function filterData(searchTerm) {
-  listItems.forEach((item) => {
-    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-      item.classList.remove("hide");
-    } else {
-      item.classList.add("hide");
-    }
-  });
-}
-
-// On click show-more functionality //
-
-showMoreBox.onClick = function () {
-  if (bottomContent.className == "open") {
-    // shrink the box //
-    bottomContent.className = "";
-    showMoreBox.innerHTML = "SHOW MORE";
-  } else {
-    // expand the box //
-    bottomContent.className = "open";
-    showMoreBox.innerHTML = "SHOW LESS";
+      result.appendChild(li);
+    });
   }
-};
+
+  function filterData(searchTerm) {
+    listItems.forEach((item) => {
+      if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+        item.classList.remove("hide");
+      } else {
+        item.classList.add("hide");
+      }
+    });
+  }
+
+  // On click show-more functionality //
+
+  showMoreBox.onclick = function () {
+    if (bottomContent.className == "open") {
+      // shrink the box //
+      bottomContent.className = "";
+      showMoreBox.innerHTML = "Show Less";
+    } else {
+      // expand the box //
+      bottomContent.className = "open";
+      showMoreBox.innerHTML = "Click For More Information";
+    }
+  };
+});
